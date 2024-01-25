cf050e896a5b916c8adaefe96890c34f8bda45b5
Renndanboi <146152897+Renndanboi@users.noreply.github.com>
2024-01-25 10:09:57 +0700

    fix: components button chat

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -18,6 +18,7 @@ const Board = () => {
   const [user, setUser] = useState();
   const [userCards, setUserCards] = useState([]);
   const { totalCards, setTotalCards, users } = useGameContext();
+  const [toogle, setToogle] = useState(false)
 
   const handleUserCards = () => {
     setUserCards(totalCards && totalCards.slice(0, 6));
@@ -32,6 +33,42 @@ const Board = () => {
   }, [users]);
 
   return (
+    // <>
+    //   <div className="w-screen h-screen bg-main-board bg-cover bg-center">
+    //     <div className="relative flex items-center justify-center">
+    //       <div className="text-center">
+    //         <h1 className="text-4xl font-bold mb-6 text-yellow-300">
+    //           Welcome on Board {user && user.userName}
+    //         </h1>
+    //         <p className="text-gray-50 italic">
+    //           Players:{" "}
+    //           {users?.map(
+    //             (user, idx) => (idx !== 0 ? ", " : "") + user.userName
+    //           )}
+    //         </p>
+    //         {users.length &&
+    //           users.map((user) => (
+    //             <h3 key={user.userName} className="text-gray-50">
+    //               {user.userName}
+    //             </h3>
+    //           ))}
+    //         <button
+    //           className="btn btn-error  bg-gradient-to-r hover:from-red-500 hover:to-yellow-500 italic"
+    //           onClick={handleUserCards}
+    //         >
+    //           Distribute Cards
+    //         </button>
+
+    //         <Table/>
+    //       </div>
+    //     </div>
+    //     <Chat />
+    //     <div className="fixed bottom-0 right-0 p-4">
+    //     <Hand userCards={userCards} setUserCards={setUserCards} />
+    //   </div>
+    //   </div>
+    // </>
+
     <>
       <div className="w-screen h-screen bg-main-board bg-cover bg-center">
         <div className="relative flex items-center justify-center">
@@ -61,7 +98,15 @@ const Board = () => {
             <Table/>
           </div>
         </div>
-        <Chat />
+        {/* {ini toogle} */}
+        <div className="fixed top-0 left-0 p-10">
+        {toogle?<Chat />:null}
+        {/* <button onClick={()=>setToogle(!toogle)}>CHAT^</button>
+         */}
+          <button onClick={() => setToogle(!toogle)}
+          className="bg-red-400 hover:bg-yellow-500 text-black font-bold py-2 px-4 rounded">Chat</button>
+        </div>
+
         <div className="fixed bottom-0 right-0 p-4">
         <Hand userCards={userCards} setUserCards={setUserCards} />
       </div>
